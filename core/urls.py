cc5adb323cdc472b8c40d2a84f0033d9698b787d
bositxonn <londonpreff@gmail.com>
2026-03-29 18:46:54 +0500

    feat: built premium frontend UI

--- core/urls.py
+++ core/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/courses/', include('apps.courses.urls')),
     path('api/users/', include('apps.users.urls')),
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),
 ]
